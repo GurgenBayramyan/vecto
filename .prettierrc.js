b8c2c8caec732c1f6d15d34896f4d879666d7f2c
GurgenBayramyan <gugobayramyan@gmail.com>
2024-10-13 02:33:04 +0400

    Updates

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	arrowParens: 'avoid',
+	doubleQuote: true,
+	trailingComma: 'all',
+	tabWidth: 4,
+	endOfLine: 'auto',
+	semi: true,
+	trailingComma: 'all',
+	arrowParens: 'always'
+};
